7d3deabd0430132143bc41d76d67ab3851312d97
PersieDaCoder <thinhdpham2510@gmail.com>
2025-03-21 15:32:02 +0700

    update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,25 +1,6 @@
-local debounce_timer = vim.loop.new_timer()
-
 vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = { "*" },
     callback = function()
         vim.cmd("silent! w")
-    end,
-})
-
-vim.api.nvim_create_autocmd("TextChanged", {
-    pattern = { "*" },
-    callback = function()
-        if debounce_timer then
-            debounce_timer:stop()
-        end
-        -- Start a new debounce timer (e.g., 300ms)
-        debounce_timer:start(
-            300,
-            0,
-            vim.schedule_wrap(function()
-                vim.cmd("silent! w")
-            end)
-        )
     end,
 })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,22 @@
 return {
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("cyberdream").setup({
-        -- Enable transparent background
-        transparent = true,
-        -- Disable or enable colorscheme extensions
-        extensions = {
-          telescope = true,
-          notify = true,
-          mini = true,
-          snacks = true,
-          -- ...
-        },
-      })
-
-      vim.cmd([[colorscheme cyberdream]])
-    end,
-  },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("cyberdream").setup({
+                -- Enable transparent background
+                transparent = true,
+                -- Disable or enable colorscheme extensions
+                extensions = {
+                    telescope = true,
+                    notify = true,
+                    mini = true,
+                    snacks = true,
+                    -- ...
+                },
+            })
+            vim.cmd([[colorscheme cyberdream]])
+        end,
+    },
 }
