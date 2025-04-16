b767b7f7397fa6e9983b0dfdf516122868d4956d
PersieDaCoder <thinhdpham2510@gmail.com>
2025-04-16 16:34:58 +0700

    revert

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,22 @@
 return {
     {
-        "datsfilipe/vesper.nvim",
+        "scottmckendry/cyberdream.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            require("vesper").setup({
-                transparent = true, -- Boolean: Sets the background to transparent
-                italics = {
-                    comments = true, -- Boolean: Italicizes comments
-                    keywords = false, -- Boolean: Italicizes keywords
-                    functions = false, -- Boolean: Italicizes functions
-                    strings = true, -- Boolean: Italicizes strings
-                    variables = false, -- Boolean: Italicizes variables
+            require("cyberdream").setup({
+                -- Enable transparent background
+                transparent = true,
+                -- Disable or enable colorscheme extensions
+                extensions = {
+                    telescope = true,
+                    notify = true,
+                    mini = true,
+                    snacks = true,
+                    -- ...
                 },
-                overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
-                palette_overrides = {},
             })
-            vim.cmd([[colorscheme vesper]])
+            vim.cmd([[colorscheme cyberdream]])
         end,
     },
 }
